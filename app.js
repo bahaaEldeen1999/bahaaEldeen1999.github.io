0868df6ac6ba9dba7254335e5129b25d45c4c484
bahaaEldeen <bahaaeldeen1999@gmail.com>
2020-09-17 17:09:06 +0200

    remove animations

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
+// let cols = document.querySelectorAll('.col')
+// let headers = document.querySelectorAll('h1,h2,h3,.main-header,p');
+// let i =0;
+// for(let col of cols){
+//     if(i > 1) col.setAttribute('data-aos','zoom-in');
+//     i++;
+// }
+// i=0;
+// for(let h of headers){
 
-
-let cols = document.querySelectorAll('.col')
-let headers = document.querySelectorAll('h1,h2,h3,.main-header,p');
-let i =0;
-for(let col of cols){
-    if(i > 1) col.setAttribute('data-aos','zoom-in');
-    i++;
-}
-i=0;
-for(let h of headers){
-
-   if(i > 1) h.setAttribute('data-aos','flip-up');
-    i++;
-}
-AOS.init({ once: true});
+//    if(i > 1) h.setAttribute('data-aos','flip-up');
+//     i++;
+// }
+// AOS.init({ once: true});
